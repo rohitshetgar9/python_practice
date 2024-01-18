75054ca7a7f2312b9198939284d9de9b26651b17
rohitshetgar9 <rohitshetgar9@gmail.com>
2024-01-18 22:41:56 +0530

    Create python_practice.py

---
+++ python_practice.py
@@ -0,0 +1,26 @@
+l1 = [2, 4, 3]
+l2 = [5, 6, 4]
+l3 = []
+
+carry = 0
+
+for i in range(len(l1)):
+    total = l1[i] + l2[i] + carry
+    digit = total % 10
+    carry = total // 10
+    l3.append(digit)
+
+print(l3)
+
+
+
+
+
+
+
+
+
+
+
+
+
